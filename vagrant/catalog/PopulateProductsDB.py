c4a38c62d70c6c863d6296a739b8e02f025fa139
zacktwp <zacktwp@gmail.com>
2019-05-17 11:22:39 -0700

    Created Database and populated it

---
+++ vagrant/catalog/PopulateProductsDB.py
@@ -0,0 +1,32 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from database_setup import ProductCatagory, Base, Product
+
+engine = create_engine('sqlite:///Product.db')
+# Bind the engine to the metadata of the Base class so that the
+# declaratives can be accessed through a DBSession instance
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+# A DBSession() instance establishes all conversations with the database
+# and represents a "staging zone" for all the objects loaded into the
+# database session object. Any change made against the objects in the
+# session won't be persisted into the database until you call
+# session.commit(). If you're not happy about the changes, you can
+# revert all of them back to the last commit by calling
+# session.rollback()
+session = DBSession()
+
+# Products for Groceries
+ProductCatagory1 = ProductCatagory(name="Groceries")
+
+session.add(ProductCatagory1)
+session.commit()
+
+Product2 = Product(name="Tuna", description="Delicious Blue Fine Tuna, extra Dolphin",
+                    price = "1.23", ProductCatagory=ProductCatagory1)
+session.add(Product2)
+session.commit()
+
+print ("added products to DB!")
---
+++ vagrant/catalog/database_setup.py
@@ -0,0 +1,32 @@
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class ProductCatagory(Base):
+    __tablename__ = 'ProductCatagory'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+
+class Product(Base):
+    __tablename__ = 'Product'
+
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    description = Column(String(250))
+    price = Column(String(8))
+    ProductCatagory_id = Column(Integer, ForeignKey('ProductCatagory.id'))
+    ProductCatagory = relationship(ProductCatagory)
+
+
+engine = create_engine('sqlite:///Product.db')
+
+
+Base.metadata.create_all(engine)
