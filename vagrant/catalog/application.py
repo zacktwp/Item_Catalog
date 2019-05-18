cdd642d26a9345e06875c37e8dad5e70ac84fb73
zacktwp <zacktwp@gmail.com>
2019-05-18 16:37:13 -0700

    added new, edit and delete pages

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -10,7 +10,7 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-
+@app.route('/')
 @app.route('/ProductCatagory/<int:ProductCatagory_id>/')
 def ProductCat(ProductCatagory_id):
     product_cat = session.query(ProductCatagory).filter_by(id=ProductCatagory_id).one()
@@ -23,6 +23,24 @@ def ProductCat(ProductCatagory_id):
         output += '</br>'
         output += i.description
     return output
+
+# Task 1: Create route for newMenuItem function here
+
+@app.route('/ProductCatagory/<int:ProductCatagory_id>/new/')
+def newProductItem(ProductCatagory_id):
+    return "page to create a new menu item. Task 1 complete!"
+
+# Task 2: Create route for editMenuItem function here
+
+@app.route('/ProductCatagory/<int:ProductCatagory_id>/<int:Product_id>/edit/')
+def editProductItem(ProductCatagory_id, Product_id):
+    return "page to edit a menu item. Task 2 complete!"
+
+# Task 3: Create a route for deleteMenuItem function here
+
+@app.route('/ProductCatagory/<int:ProductCatagory_id>/<int:Product_id>/delete/')
+def deleteProductItem(ProductCatagory_id, Product_id):
+    return "page to delete a menu item. Task 3 complete!"
 
 if __name__ == '__main__':
     app.debug = True
