dc5bf3144c4b9ce5ccd17c3006e6b98a3787744d
zacktwp <zacktwp@gmail.com>
2019-05-18 13:46:43 -0700

    changed routing in flask

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -11,15 +11,17 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 
-@app.route('/')
-@app.route('/hello')
-def HelloWorld():
-    product_cat = session.query(ProductCatagory).first()
+@app.route('/ProductCatagory/<int:ProductCatagory_id>/')
+def ProductCat(ProductCatagory_id):
+    product_cat = session.query(ProductCatagory).filter_by(id=ProductCatagory_id).one()
     prod = session.query(Product).filter_by(ProductCatagory_id=ProductCatagory.id)
     output = ''
     for i in prod:
         output += i.name
         output += '</br>'
+        output += i.price
+        output += '</br>'
+        output += i.description
     return output
 
 if __name__ == '__main__':
