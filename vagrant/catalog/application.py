6e7aa066ba451ceff8e9f6bfa011ba87ad057bae
zacktwp <zacktwp@gmail.com>
2019-05-22 07:14:12 -0700

    access on port 8000

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -82,4 +82,4 @@ def deleteProductItem(ProductCatagory_id, Product_id):
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     app.debug = True
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8000)
